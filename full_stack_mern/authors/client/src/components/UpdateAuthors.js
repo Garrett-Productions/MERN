379fc212feefc3966d7693f3b292033b0fd86887
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-04 15:03:38 -1000

    first commit

--- full_stack_mern/authors/client/src/components/UpdateAuthors.js
+++ full_stack_mern/authors/client/src/components/UpdateAuthors.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import axios from 'axios'
 import {useNavigate, Link, useParams} from 'react-router-dom'
 
 const UpdateAuthors = ({authorList, setAuthorList, errors, setErrors}) => {

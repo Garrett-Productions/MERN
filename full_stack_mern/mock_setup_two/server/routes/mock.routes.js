13bb5dde7bc422aec2f388d6a83d1d8f76569c85
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-10 09:26:19 -1000

    first commit

---
+++ full_stack_mern/mock_setup_two/server/routes/mock.routes.js
@@ -0,0 +1,7 @@
+const AuthorController = require('../controllers/author.controller');
+module.exports = app => {
+
+    app.get('/api/authors', AuthorController.getAllAuthors);
+    app.post('/api/authors', AuthorController.createAuthor);
+    app.get('/api/authors/:id', AuthorController.getOneAuthor);
+}

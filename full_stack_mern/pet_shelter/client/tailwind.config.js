2ba3df25c3abf3c5cb2888c3c2b6aa11ea6bdd4b
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-12-19 19:27:03 -0500

    first commit

--- full_stack_mern/pet_shelter/client/tailwind.config.js
+++ full_stack_mern/pet_shelter/client/tailwind.config.js
@@ -15,3 +15,18 @@ export default {
   plugins: [],
 }
 
+// 1. npm create vite@latest client — —template react
+// 2. Cd client
+// 3. npm install (installs vite dependencies)
+// 4. npm install -D tailwindcss postcss autoprefixer
+// 5. npx tailwindcss init -p
+// 6. Edit tailwind.config file
+// content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+// 6. Edit index.css file
+// @tailwind base;
+// @tailwind components;
+// @tailwind utilities;
+// 7. npm run dev
